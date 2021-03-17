f1ee73a0e0ba8a86a36a78a5af47241b8851fece
kamrul <kamrul1157024@gmai.com>
2021-03-17 08:44:08 +0600

    Added Heroku Dependencies

--- TextAnalyzer/settings.py
+++ TextAnalyzer/settings.py
@@ -85,6 +85,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
+import django_heroku
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
